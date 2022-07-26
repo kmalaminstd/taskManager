67c2519fe02b264239c11ed186e6f8b388ef6a15
kmalamin <alaminkhanstd@gmail.com>
2022-07-26 17:15:16 +0600

    fix all bugs of before days with edit button half functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
-class All{
+let taskArr = []
+const localStorageInit = {
+    
+    sendTaskLocalStorage(taskObj){
+    let localTaskArr = [];
+        
+        if(localStorage.getItem('Task')){
+
+            localTaskArr = JSON.parse(localStorage.getItem('Task'))
+            localTaskArr.push(taskObj)
+            localStorage.setItem('Task',JSON.stringify(localTaskArr))
+
+        }else{
+            localTaskArr = []
+            localTaskArr.push(taskObj)
+            localStorage.setItem('Task', JSON.stringify(localTaskArr))
+        }
+        
+    }
+}
+
+const all = {
     allSelectors(){
         const taskTitle = document.querySelector('#taskTitle')
         const taskSubTitle = document.querySelector('#subTitle')
@@ -39,9 +60,7 @@ class All{
             taslRangeELm,
             taskRnageShowFiled
         }
-    }
-
-
+    },
 
     taskRangeShow(){
         const {taskRnageShowFiled, taslRangeELm} = this.allSelectors()
@@ -49,14 +68,9 @@ class All{
         taslRangeELm.addEventListener('change', e => {
             taskRnageShowFiled.value = e.target.value;
         })
-    }
+    },
 
-    initialize(){
-
-        
-
-        let taskArr = []
-        
+    initialize(){ 
         const {form} = this.allSelectors()
 
         form.addEventListener('submit', e => {
@@ -71,13 +85,78 @@ class All{
                 const {taskTitleValue,taskSubTitleValue,assignedToValue,taskStartDateValue,taskEndDateValue, priorityValue, taskStatusValue ,taslRangeELmValue } = this.storeAllValue()
 
                 this.dataInsertField(taskTitleValue,taskSubTitleValue,assignedToValue,taskStartDateValue,taskEndDateValue,priorityValue, taskStatusValue, taslRangeELmValue)
-            }
-            const dataObj = this.dataStoreInArray()
 
-            taskArr.push(dataObj)
-            this.dataSendLocalStorage(taskArr)
-        })           
-    }
+                
+
+            }
+            
+        })   
+        
+        document.addEventListener('DOMContentLoaded', () => {
+            
+            if(localStorage.getItem('Task')){
+                const taskFromStorage = JSON.parse(localStorage.getItem('Task'))
+
+            taskFromStorage.map( task => {
+                const tr = document.createElement('tr')
+
+                tr.innerHTML = `
+            <td id="taskId">${task.TaskId}</td>
+            <td>${task.taskTitle}</td>
+            <td>${task.Priority}</td>
+            <td>${task.Status}</td>
+            <td>${task.EndDate}</td>
+            <td>${task.Assigned}</td>
+            <td>${task.CompleteRange}%</td>
+            <td><i id="editBtn" class="fa fa-pencil-square-o" aria-hidden="true"></i> <i id="completeBtn" class="fa fa-check-square" aria-hidden="true"></i>
+            <i id="deleteBtn" class="fa fa-trash" aria-hidden="true"></i></td>
+                `
+                document.querySelector('table tbody').appendChild(tr)
+            })
+            }
+            // =================
+            const allTask = JSON.parse(localStorage.getItem('Task'))
+            const deleteBtn = document.querySelectorAll('#deleteBtn');
+            const editBtn = document.querySelectorAll('#editBtn');
+            const completeBtn = document.querySelectorAll('#completeBtn')
+    
+            const {taskTitle, taskSubTitle, assignedTo, taskStartDate, taskEndDate,taskPriorityElm,taskStatusElm, taslRangeELm} = this.allSelectors()
+
+            for(let i = 0; i < editBtn.length; i++){
+                editBtn[i].addEventListener('click', () => {
+                    document.querySelector('#updateBtnElm').style.display = "inline-block"
+                    const allTaskId = document.querySelectorAll('#taskId')
+                   allTask.find( elem => {
+                    if(elem.TaskId === Number(allTaskId[i].textContent)){
+                        
+                        taskTitle.value = elem.taskTitle
+                        taskSubTitle.value = elem.taskSubTitle
+                        assignedTo.value = elem.Assigned
+                        taskStartDate.value = elem.StartDate
+                        taskEndDate.value = elem.EndDate
+                        taskPriorityElm.value = elem.Priority
+                        taskStatusElm.value = elem.Status
+                        taslRangeELm.value = elem.CompleteRange
+                    }
+                   })
+                })
+            }
+
+            for(let i = 0; i < deleteBtn.length; i++){
+                deleteBtn[i].addEventListener('click', () => {
+                    const allTaskId = document.querySelectorAll('#taskId')
+                    
+                })
+            }
+
+            for(let i = 0; i < completeBtn.length; i++){
+                completeBtn[i].addEventListener('click', () => {
+                    const allTaskId = document.querySelectorAll('#taskId')
+                })
+            }  
+
+        })
+    },
 
     dataSendLocalStorage(taskArr){
         // console.log(taskArr);
@@ -97,14 +176,16 @@ class All{
 
 
         
-    }
+    },
 
     dataInsertField(taskTitleValue,taskSubTitleValue,assignedToValue,taskStartDateValue,taskEndDateValue,  priorityValue, taskStatusValue, taslRangeELmValue){
+
+        
+
         const tr = document.createElement('tr')
 
         const taskId = document.querySelectorAll('#taskId').length+1;
 
-        
 
         tr.innerHTML = `
             <td id="taskId">${taskId}</td>
@@ -118,24 +199,20 @@ class All{
             <i id="deleteBtn" class="fa fa-trash" aria-hidden="true"></i></td>
         `
         document.querySelector('table tbody').appendChild(tr)
+         // ===================
 
-        return taskId
 
-    }
+        const deleteBtn = document.querySelectorAll('#deleteBtn');
+        const editBtn = document.querySelectorAll('#editBtn');
+        const completeBtn = document.querySelectorAll('#completeBtn')
 
-    dataStoreInArray(){
+        return {deleteBtn, editBtn, completeBtn}
 
-        const taskId = this.dataInsertField()
+    },
+
+   
+    dataStoreInObj(taskId,taskTitleValue,taskSubTitleValue,assignedToValue,taskStartDateValue,taskEndDateValue,  priorityValue, taskStatusValue, taslRangeELmValue){
         
-        const {taskTitleValue,
-            taskSubTitleValue,
-            assignedToValue,
-            taskStartDateValue,
-            taskEndDateValue,
-            priorityValue,
-            taskStatusValue,
-            taslRangeELmValue} = this.storeAllValue();
-
         let dataObj = {
             TaskId : taskId,
             taskTitle : taskTitleValue,
@@ -147,11 +224,11 @@ class All{
             Status : taskStatusValue,
             CompleteRange : taslRangeELmValue
         }
-
         // console.log(dataObj);
-
-        return dataObj
-    }
+        
+        taskArr.push(dataObj)
+        
+    },
 
     validationField(){
 
@@ -166,7 +243,7 @@ class All{
         }
 
         return isError
-    }
+    },
 
     storeAllValue(){
        
@@ -207,6 +284,5 @@ class All{
     }
 }
 
-const all = new All()
 all.initialize()
 all.taskRangeShow()
